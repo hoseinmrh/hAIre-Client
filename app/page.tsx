b6755a3049b9ea3cb467627ac77ce1a1772a7986
Hosein Mirhoseini <hoseinmirhoseini64@gmail.com>
2024-10-22 09:54:04 +0200

    :art: added delete all questions, some style change, added next button

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
-import { RiUploadLine } from "react-icons/ri";
-import { useRef } from "react";
+import { RiCornerDownRightLine, RiUploadLine } from "react-icons/ri";
+import { useRef, useState } from "react";
 
 export default function Home() {
   const inputFileRef = useRef<HTMLInputElement | null>(null);
+  const [filename, setFilename] = useState<string>("");
 
   const handleClick = () => {
     if (inputFileRef.current) {
@@ -16,15 +17,23 @@ export default function Home() {
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files ? event.target.files[0] : null;
     if (file) {
-      console.log("Selected file:", file);
+      setFilename(file.name);
       // Handle the uploaded file here (e.g., upload it to the server or process it)
     }
+  };
+
+  const handleNext = () => {
+    if (!filename) {
+      alert("Please Add Your CV");
+      return;
+    }
+    console.log("meow");
   };
   return (
     <main className="bg-black text-orange-500 h-full">
       <div className="container max-w-3xl m-auto">
         <div className="flex flex-col items-center justify-between p-10 mt-20">
-          <Image src={"/logo2.png"} alt={"logo"} width="400" height="200" />
+          <Image src={"/logo2.png"} alt={"logo"} width="300" height="200" />
 
           <TypeAnimation
             sequence={[
@@ -37,31 +46,51 @@ export default function Home() {
             speed={60}
             style={{ display: "inline-block" }}
             repeat={1}
-            className="text-center mt-28 text-5xl"
+            className="text-center mt-28 text-4xl"
             cursor={false}
           />
-          <div>
-            <button
-              onClick={handleClick}
-              className="relative bg-black text-orange-500 py-3 px-10 rounded-3xl
+          <div className="flex flex-row space-x-20">
+            <div>
+              <button
+                onClick={handleClick}
+                className="relative bg-black text-orange-500 py-2 w-48 rounded-3xl
             drop-shadow-[0_2px_5px_rgba(255,165,0,0.8)]
             focus:outline-none hover:drop-shadow-[0_3px_10px_rgba(255,165,0,1)]
-             text-4xl mt-28"
-            >
-              <RiUploadLine
-                size="48px"
-                style={{ display: "inline" }}
-                className="ml-2"
-              />{" "}
-              Upload CV
-            </button>
-            <input
-              ref={inputFileRef}
-              type="file"
-              accept="application/pdf"
-              className="hidden"
-              onChange={handleFileChange}
-            />
+             text-xl mt-28"
+              >
+                <RiUploadLine
+                  size="48px"
+                  style={{ display: "inline" }}
+                  className="ml-2"
+                />{" "}
+                Upload CV
+              </button>
+              <input
+                ref={inputFileRef}
+                type="file"
+                accept="application/pdf"
+                className="hidden"
+                onChange={handleFileChange}
+              />
+              <div className="mt-2">{filename}</div>
+            </div>
+
+            <div>
+              <button
+                onClick={handleNext}
+                className="relative bg-orange-500 text-black py-2 w-48 rounded-3xl
+            drop-shadow-[0_2px_5px_rgba(255,165,0,0.8)]
+            focus:outline-none hover:drop-shadow-[0_3px_10px_rgba(255,165,0,1)]
+             text-xl mt-28"
+              >
+                <RiCornerDownRightLine
+                  size="48px"
+                  style={{ display: "inline" }}
+                  className="ml-2"
+                />{" "}
+                Next
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- app/questionnaire/page.tsx
+++ app/questionnaire/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
-import { RiAddLine, RiCheckLine, RiUploadLine } from "react-icons/ri";
+import { RiAddLine, RiCheckLine, RiCloseLine } from "react-icons/ri";
 import { useRef, useState } from "react";
 
 export default function Home() {
@@ -24,6 +24,10 @@ export default function Home() {
     }
   };
 
+  const handleDelete = () => {
+    setQuestions([]);
+  };
+
   const handleSubmit = () => {
     console.log(questions);
   };
@@ -42,7 +46,7 @@ export default function Home() {
             speed={60}
             style={{ display: "inline-block" }}
             repeat={Infinity}
-            className="text-center mt-28 text-5xl"
+            className="text-center mt-28 text-4xl"
             cursor={false}
           />
           <textarea
@@ -53,16 +57,27 @@ export default function Home() {
             rows={1}
             ref={questionTextRef}
           />
-          <div className="w-full flex justify-end">
+          <div className="w-full flex justify-end space-x-8">
             <button
               onClick={handleAdd}
-              className="bg-black text-orange-500 py-2 px-5 rounded-3xl
+              className="bg-black text-orange-500 py-2 w-40 rounded-3xl
             drop-shadow-[0_1px_3px_rgba(255,165,0,0.8)]
             focus:outline-none hover:drop-shadow-[0_2px_5px_rgba(255,165,0,1)]
-             text-lg mt-10"
+             text-md mt-10"
             >
               <RiAddLine style={{ display: "inline" }} className="ml-2" /> Add
               Question
+            </button>
+
+            <button
+              onClick={handleDelete}
+              className="bg-red-500 text-white py-2 w-40 rounded-3xl
+            drop-shadow-[0_1px_3px_rgba(255,165,0,0.8)]
+            focus:outline-none hover:drop-shadow-[0_2px_5px_rgba(255,165,0,1)]
+             text-md mt-10"
+            >
+              <RiCloseLine style={{ display: "inline" }} className="ml-2" />{" "}
+              Delete All
             </button>
           </div>
           <div className="w-full flex justify-start">
@@ -72,13 +87,24 @@ export default function Home() {
               ))}
             </ul>
           </div>
+          <hr />
+          <div className="w-full flex flex-col justify-start text-gray-500">
+            <div className="text-2xl font-bold mb-4 mt-8">
+              Some recommended questions:
+            </div>
+            <ul className="text-xl list-disc pl-5">
+              <li key={0}>Where do you see yourself in the next 5 years?</li>
+              <li key={0}>What is your biggest achievement?</li>
+              <li key={0}>Who are your role models?</li>
+            </ul>
+          </div>
 
           <button
             onClick={handleSubmit}
-            className="relative bg-black text-orange-500 py-3 px-10 rounded-3xl
+            className="relative bg-orange-500 text-black py-2 w-48 rounded-3xl
             drop-shadow-[0_2px_5px_rgba(255,165,0,0.8)]
             focus:outline-none hover:drop-shadow-[0_3px_10px_rgba(255,165,0,1)]
-             text-4xl mt-24 mb-12"
+             text-xl mt-24 mb-12"
           >
             <RiCheckLine
               size="48px"
